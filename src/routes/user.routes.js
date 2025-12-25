d02f98c968c7df78f10504e8a735699e6026f564
Rudrajyoty Mahata <rudrajyotymahata@gmail.com>
2025-12-25 12:22:54 +0530

    Refactor user routes to include password change, profile retrieval, account update, and image upload functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser , refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser,
+    logoutUser , 
+    refreshAccessToken, 
+    chnageCurrentUserPassword, 
+    getCurrentUserProfile, 
+    updateAccountDetails ,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -18,5 +30,24 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/chnage-password").post(verifyJWT, chnageCurrentUserPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUserProfile);
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+);
+
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
